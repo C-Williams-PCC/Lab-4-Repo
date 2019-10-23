026f47875511ac663ab86ed11c286620fe61ea3c
Cassandra Williams <cassandra.williams1@pcc.edu>
2019-10-22 21:36:52 -0700

    Added functionality to print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,5 @@
 package edu.pcc.cis.helloworld;
+import java.util.Date;
 
 /**
  * Created by Cassandra Williams (C-Williams-PCC) on 10/22/19
@@ -7,5 +8,6 @@ package edu.pcc.cis.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(new Date());
     }
 }
