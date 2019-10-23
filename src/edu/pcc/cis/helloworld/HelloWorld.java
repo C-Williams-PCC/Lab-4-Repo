69cd5796d3e9e2c6eaea62c6f1959f46538de36e
Cassandra Williams <cassandra.williams1@pcc.edu>
2019-10-22 21:27:42 -0700

    Added main method which prints Hello World!

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,11 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by Cassandra Williams (C-Williams-PCC) on 10/22/19
+ */
+
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
